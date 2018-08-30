5040a7174d2ded5a6444f3af7d749a07fbe7f3e5
Damian Arellanes <damian.arellanesmolina@manchester.ac.uk>
2018-08-30 15:20:12 +0100

    * CHECKPOINT Selector now evaluates conditions in parallel for multiple branches.
    The next steps are:
    
    0. Add looper.
    1. Optimise DXManWorkflowTreeDeployer
    2. Read multiple parameter values at once rather than one at a time
    3. Sort out other TODOs (find them in the project)

--- thing/src/main/java/com/dxman/thing/deployment/connectors/composite/DXManSelectorInstance.java
+++ thing/src/main/java/com/dxman/thing/deployment/connectors/composite/DXManSelectorInstance.java
@@ -1,7 +1,8 @@
 package com.dxman.thing.deployment.connectors.composite;
 
-import com.dxman.design.services.composite.DXManCompositeServiceTemplate;
 import com.dxman.execution.common.DXManWfNodeMapper;
+import com.dxman.design.connectors.composition.*;
+import com.dxman.design.services.composite.DXManCompositeServiceTemplate;
 import com.dxman.execution.common.DXManWfCondition;
 import com.dxman.execution.selector.DXManWfSelector;
 import com.dxman.execution.selector.DXManWfSelectorCustom;
@@ -9,14 +10,125 @@ import com.dxman.thing.deployment.connectors.common.DXManConnectorDataManager;
 import com.dxman.thing.deployment.connectors.common.DXManConnectorInstance;
 import com.dxman.thing.server.base.DXManConnectorRequester;
 import com.google.gson.Gson;
-import java.util.Date;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * @author Damian Arellanes
  */
 public class DXManSelectorInstance extends DXManConnectorInstance {
   
+  private class ConcurrentManager {
+    
+    public ConcurrentManager() {}
+
+    public void sync() {
+      ExecutorService executor = executeParallelChilds();
+      joinParallelChilds(executor);
+    }
+
+    public void async() {
+      executeParallelChilds();
+    }
+
+    private ExecutorService executeParallelChilds() {
+
+      // Adds all the tasks into a set
+      Set<Callable<String>> tasks = new HashSet<>();
+      
+      for(DXManWfNodeMapper subNodeMapper: subNodeMappers) {
+        
+        DXManWfCondition condition = ((DXManWfSelectorCustom)subNodeMapper
+          .getCustom()).getCondition();
+        
+        if(connectorDataManager.matches(workflowId, 
+          workflowTimestamp, condition)) {
+          
+          tasks.add((Callable) () -> {          
+            transferControl(
+              subNodeMapper.getNode(), subNodeMapper.getNode().getUri()
+            );
+            return null;
+          });
+        }
+      }
+      
+      // Pool size = sum of tasks in the subnodes of the workflow tree
+      ExecutorService executor = Executors.newFixedThreadPool(tasks.size());
+      
+      // Invokes all tasks in parallel
+      try {
+        executor.invokeAll(tasks);
+      } catch (InterruptedException ex) {
+        System.err.println(ex);
+      }
+      
+      // Terminates threads for the tasks
+      executor.shutdown();
+      
+      return executor;
+    }
+
+    private void joinParallelChilds(ExecutorService executor) {
+      while (!executor.isTerminated()) {}
+    }
+  }
+  
+  private interface Invocation {
+    public void execute();
+  }
+        
+  private class InvocationContext {
+
+    private Invocation invocation;
+
+    public InvocationContext() {}
+
+    public void execute() {
+      invocation.execute();
+    }
+
+    public void setInvocation(DXManParallelType invocationType) {
+
+      if(invocationType.equals(DXManParallelType.SYNC)) {
+        invocation = new SynchronousInvocation();
+      } else invocation = new AsynchronousInvocation();      
+    }
+  }
+    
+  private class AsynchronousInvocation implements Invocation {
+
+    private final ConcurrentManager concurrentManager;
+
+    public AsynchronousInvocation() {
+      this.concurrentManager = new ConcurrentManager();
+    }
+
+    @Override
+    public void execute() {
+      concurrentManager.async();
+    }
+  }
+
+  private class SynchronousInvocation implements Invocation {
+
+    private final ConcurrentManager concurrentManager;
+
+    public SynchronousInvocation() {
+      this.concurrentManager = new ConcurrentManager();
+    }
+
+    @Override
+    public void execute() {
+      concurrentManager.sync();
+    }
+  }
+  
+  private final InvocationContext invocationContext;
+  private List<DXManWfNodeMapper> subNodeMappers;
   private final DXManConnectorDataManager connectorDataManager;
+  private String workflowId;
+  private String workflowTimestamp;    
 
   public DXManSelectorInstance(DXManCompositeServiceTemplate managedService, 
     DXManConnectorDataManager connectorDataManager, 
@@ -26,6 +138,9 @@ public class DXManSelectorInstance extends DXManConnectorInstance {
       requester, gson);
     
     this.connectorDataManager = connectorDataManager;
+    
+    invocationContext = new InvocationContext();    
+    invocationContext.setInvocation(DXManParallelType.SYNC);
   }
 
   @Override
@@ -35,20 +150,10 @@ public class DXManSelectorInstance extends DXManConnectorInstance {
       + " (Selector Connector) activated [" + new Date() + "]");
     
     DXManWfSelector flow = gson.fromJson(workflowJSON, DXManWfSelector.class);
+    subNodeMappers = flow.getSubnodeMappers();
+    workflowId = flow.getWorkflowId();
+    workflowTimestamp = flow.getWorkflowTimestamp();
     
-    for(DXManWfNodeMapper subNodeMapper: flow.getSubnodeMappers()) {
-      
-      DXManWfCondition condition = ((DXManWfSelectorCustom)subNodeMapper
-        .getCustom()).getCondition();
-      
-      if(connectorDataManager.matches(flow.getWorkflowId(), 
-        flow.getWorkflowTimestamp(), condition)) {
-        
-        transferControl(
-          subNodeMapper.getNode(), subNodeMapper.getNode().getUri()
-        );
-      }
-    }
+    invocationContext.execute();
   }
-    
 }
